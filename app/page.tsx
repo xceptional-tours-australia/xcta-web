dda58feb3fe76674ce26770bd592005b4c6aa8ab
Galvin Santoso <galvinsantoso22@gmail.com>
2024-09-03 02:16:01 +0700

    advantages done

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import "./style.css"
 import Hero from '@/components/Hero/Hero'
 import AboutSection from '@/components/AboutSection/AboutSection';
+import Advantages from '@/components/Advantages/Advantages';
+
 
 export default function Home() {
   return (
     <main className="home">
       <Hero />
       <AboutSection/>
+      <Advantages/>
     </main>
   );
 }
--- lib/dummyData.ts
+++ lib/dummyData.ts
@@ -26,5 +26,35 @@ export const abouts = [
     title: "Meet the Experts Behind Xceptional Tours Australia",
     paragraph: "At Xceptional Tours Australia (XCTA), our team is the heart and soul of our business. With an average of 18 years of experience in the travel, tourism, and aviation industries, our seasoned destination marketers are dedicated to crafting exceptional travel experiences for our clients. Each member brings a wealth of knowledge and passion to ensure that every journey is seamless, personalized, and unforgettable."
   }
-]
+];
+
+import advantageOneImg from '@/assets/image/advantage-1.jpeg';
+import advantageTwoImg from '@/assets/image/advantage-2.jpeg';
+import advantageThreeImg from '@/assets/image/advantage-3.jpeg';
+import advantageFourImg from '@/assets/image/advantage-4.jpeg';
+
+
+export const advantages = [
+    {
+        img: advantageOneImg,
+        title: "Personalization",
+        paragraph: "We tailor experiences to match individual interests and needs, creating bespoke itineraries that reflect unique preferences."
+    },
+    {
+        img: advantageTwoImg,
+        title: "Efficient Logistics",
+        paragraph: "Our expertise in handling logistics ensures a smooth and hassle-free travel experience, from arrival to departure."
+    },
+    {
+        img: advantageThreeImg,
+        title: "On-Ground Support",
+        paragraph: "We provide continuous support during your trip, addressing any issues promptly and ensuring your journey is enjoyable and stress-free."
+    },
+    {
+        img: advantageFourImg,
+        title: "Access to Exclusive Deals",
+        paragraph: "Benefit from our strong relationships with suppliers, allowing us to offer exclusive deals and promotions that provide exceptional value."
+    },
+
+];
 
