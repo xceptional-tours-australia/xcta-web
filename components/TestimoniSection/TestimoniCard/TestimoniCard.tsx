031d88c76310dd9663af07ea0af77d5cdf013f2c
Rizky Zaldi <rizkyzaldi@gmail.com>
2024-09-27 23:36:03 +0700

    feat: add scroll area

--- components/TestimoniSection/TestimoniCard/TestimoniCard.tsx
+++ components/TestimoniSection/TestimoniCard/TestimoniCard.tsx
@@ -1,23 +1,30 @@
 import { UserIcon } from "lucide-react";
 import "./style.css";
 
-export default function TestimoniCard() {
+interface TestimoniCardProps {
+  data: {
+    desc: string;
+    photo: string | undefined;
+    name: string;
+    city: string;
+  };
+}
+
+export default function TestimoniCard({
+  data: { desc, photo, name, city },
+}: TestimoniCardProps) {
   return (
     <div className="testimoni-card__wrapper space-y-16">
       <div>
-        <h2 className="testimoni-card__title">
-          “An unforgettable adventure! Xceptional Tours showed us the best of
-          Australia with fantastic guides and stunning itineraries. Highly
-          recommended!”
-        </h2>
+        <h2 className="testimoni-card__title">{`"${desc}"`}</h2>
       </div>
       <div className="testimoni-card__user-container">
         <div className="testimoni-card__user-profile-picture">
           <UserIcon />
         </div>
         <div>
-          <h3 className="testimoni-card__user-name">John Doe</h3>
-          <h5 className="testimoni-card__user-country">From New York, USA</h5>
+          <h3 className="testimoni-card__user-name">{name}</h3>
+          <h5 className="testimoni-card__user-country">{city}</h5>
         </div>
       </div>
     </div>
--- components/TestimoniSection/TestimoniSection.tsx
+++ components/TestimoniSection/TestimoniSection.tsx
@@ -1,4 +1,6 @@
+import { testimoniData } from "@/lib/dummyData";
 import SectionTitle from "../global/SectionTitle/SectionTitle";
+import { ScrollArea, ScrollBar } from "../ui/scroll-area";
 import TestimoniCard from "./TestimoniCard/TestimoniCard";
 import "./style.css";
 
@@ -6,11 +8,14 @@ export default function TestimoniSection() {
   return (
     <section className="section-wrapper py-[10rem]">
       <SectionTitle title="What They're Saying About Us" />
-      <div className="testimoni-section__container">
-        <TestimoniCard />
-        <TestimoniCard />
-        <TestimoniCard />
-      </div>
+      <ScrollArea className="w-full pb-4">
+        <div className="testimoni-section__container">
+          {testimoniData.map((data, index) => (
+            <TestimoniCard key={index} data={data} />
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </section>
   );
 }
