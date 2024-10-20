37c6216ac9a9c4b96971392c069ad7847386a1a9
Fadla Ichsan <fadla.ichsan@weekendinc.com>
2024-10-20 14:59:12 +0700

    initial our-team page

---
+++ app/our-team/page.tsx
@@ -0,0 +1,30 @@
+import Banner from "@/components/Banner/Banner"
+import "./style.css"
+import { teams } from "@/lib/dummyData"
+
+function OurTeam() {
+  return (
+    <main className="ourteam">
+      <Banner />
+
+      <section className="section-wrapper">
+        <div className="ourteam__cards">
+          {teams.map((team, i) => (
+            <div className="ourteam__card" key={i}>
+              <div className="ourteam__head">
+                <span className="ourteam__title">{ team.name }</span>
+                <span className="ourteam__subtitle">{ team.position }</span>
+              </div>
+
+              <div className="ourteam__body">
+                <p>{ team.description }</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default OurTeam
--- lib/dummyData.ts
+++ lib/dummyData.ts
@@ -158,3 +158,36 @@ export const sitemapLinks = [
     ]
   }
 ];
+
+export const teams = [
+  {
+    name: "John Doe",
+    position: "Managing Director",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    name: "John Doe",
+    position: "Managing Director",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    name: "John Doe",
+    position: "Managing Director",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    name: "John Doe",
+    position: "Managing Director",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    name: "John Doe",
+    position: "Managing Director",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    name: "John Doe",
+    position: "Managing Director",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  }
+];
