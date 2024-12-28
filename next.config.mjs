c7d30c3c064ef8b3a2c3a3124a0c54261b069833
Fadla Ichsan <fadlaichsann@gmail.com>
2024-12-28 17:07:45 +0700

    edit next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'xcta-space.sgp1.digitaloceanspaces.com',
+              pathname: '/**', // Mendukung semua jalur di domain ini
+            },
+          ],
         domains: ['xcta-space.sgp1.digitaloceanspaces.com'],
     },
 };
